2f3c4ecb979389c989345cdae65c82eef8bacd7d
varxnnnn <varunkumar615768@gmail.com>
2025-06-07 16:40:48 +0530

    Initial commit for DevOps assignment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.9-slim
+
 WORKDIR /app
+
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-COPY . .
-CMD ["python", "app.py"]
+
+COPY app/ ./app
+WORKDIR /app/app
+
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, Iam a VARUN KUMAR,I am intrested in software deployment"
+def home():
+    return "Hello from DevOps Assignment!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

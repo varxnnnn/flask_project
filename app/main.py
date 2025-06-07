2205992445d995f8ae2fd33d1f031097e4424611
varxnnnn <varunkumar615768@gmail.com>
2025-06-07 18:30:46 +0530

    7

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask # type: ignore
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello I am from Vignan"
+    return "Hello I am from Varun"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)

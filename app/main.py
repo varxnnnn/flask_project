f5ef2c1d058471b34b549a2b6cd905496674481d
varxnnnn <varunkumar615768@gmail.com>
2025-06-07 16:58:09 +0530

    6

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Assignment!"
+    return "Hello I am from Vignan"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
